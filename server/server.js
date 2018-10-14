58279b1817dd505471bee55371e1666b982513cd
Daniyal Awan <app1user11@gmail.com>
2018-10-14 23:52:29 +0500

    Added POST /users/login route

--- server/server.js
+++ server/server.js
@@ -108,6 +108,18 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
+app.post('/users/login', (req, res) => {
+  var userData = _.pick(req.body, ["email", "password"]);
+
+  User.findByCredentials(userData.email, userData.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((err) => {
+    res.status(400).send();
+  });
+});
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
